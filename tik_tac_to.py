9c2e77e396d148256e101a095485768d42720710
JSTONE2112 <jston102@students.kennesaw.edu>
2024-11-18 12:12:54 -0500

    Update tik_tac_to.py

--- tik_tac_to.py
+++ tik_tac_to.py
@@ -11,3 +11,9 @@ def check_winner(board, player):
     for row in board 
         if all(cell == player for cell in row):
             return True 
+for col in range(3):
+        if all(row[col] == player for row in board):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
